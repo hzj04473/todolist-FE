2be0499933101fe299e3451a777effd5fca25dca
hzj04473 <hzj04473@naver.com>
2025-02-03 00:04:45 +0900

    0202. 회원가입 및 로그인 1차 완료

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import { Link, useNavigate } from 'react-router-dom';
+
+import api from '../utils/api';
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigation = useNavigate();
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      if (!email) {
+        throw new Error('이매알을 입력해 주세요.');
+      }
+      if (!password) {
+        throw new Error('패스워드를 입력해 주세요.');
+      }
+
+      // api
+      const response = await api.post('/user/login', { email, password });
+      // console.log(response);
+
+      if (response.status === 200) {
+        setUser(response.data.user);
+        sessionStorage.setItem('token', response.data.token);
+        api.defaults.headers['authorization'] = 'Bearer ' + response.data.token;
+        setError('');
+        navigation('/');
+      }
+      // console.log(response);
+      throw new Error(response);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="display-center">
+      {error && <div className="red-error">{error}</div>}
+      <Form className="login-box" onSubmit={handleLogin}>
+        <h1>로그인</h1>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+          />
+        </Form.Group>
+        <div className="button-box">
+          <Button type="submit" className="button-primary">
+            Login
+          </Button>
+          <span>
+            계정이 없다면? <Link to="/register">회원가입 하기</Link>
+          </span>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
 export default LoginPage;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,90 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import api from '../utils/api';
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [secPassword, setSecpassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if (password !== secPassword) {
+        throw new Error('패스워드가 일치 하지 않습니다. 다시 입력해 주세요.');
+      }
+      // api
+      const response = await api.post('/user', {
+        name,
+        email,
+        password,
+      });
+      // console.log(response);
+      if (response.status === 201) {
+        navigate('/login');
+      } else {
+        throw new Error(response.data.error);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="display-center">
+      {error && <div className="red-error">{error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit}>
+        <h1>회원가입</h1>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="string"
+            placeholder="Name"
+            onChange={(event) => setName(event.target.value)}
+            value={name}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>re-enter the password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="re-enter the password"
+            onChange={(event) => setSecpassword(event.target.value)}
+            value={secPassword}
+          />
+        </Form.Group>
+
+        <Button className="button-primary" type="submit">
+          회원가입
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default RegisterPage;
