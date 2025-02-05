2b0012c6bebe1d2d10591e4218650aa6d25ec3e0
hzj04473 <hzj04473@naver.com>
2025-02-05 11:27:49 +0900

    0204. 네비게이션 파일 소스 수정

--- src/pages/NavPage.js
+++ src/pages/NavPage.js
@@ -8,49 +8,59 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 export const NavPage = () => {
+  const handleLogout = () => {
+    try {
+      sessionStorage.removeItem('token');
+
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
   return (
     <>
-      {['md'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-          <Container fluid>
-            <Navbar.Brand href="/">Todo List</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Todo List
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="/">나의 할일</Nav.Link>
+      <Navbar key={`md`} expand={`md`} className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Brand href="/">Todo List</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-md`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+                Todo List
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="/">나의 할일</Nav.Link>
 
-                  <NavDropdown
-                    title="마이페이지"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#">회원정보</NavDropdown.Item>
-                    <NavDropdown.Item href="/login">로그아웃</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="할일 검색"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-dark">🔎</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+                <NavDropdown
+                  title="마이페이지"
+                  id={`offcanvasNavbarDropdown-expand-md`}
+                >
+                  <NavDropdown.Item href="#">회원정보</NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    로그아웃
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="할일 검색"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-dark">🔎</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </>
   );
 };
