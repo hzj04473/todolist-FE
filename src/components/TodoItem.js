590f2c541a3655792260fe79d43d26d2dcc8f392
hzj04473 <hzj04473@naver.com>
2025-02-10 17:57:38 +0900

    0210. 투두리스트 제목 높이 고정 수치 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,9 @@ const TodoItem = ({ item, onDelete, toggleCompleteTask, formatDate }) => {
     <Col xs={12} sm={6} md={4}>
       <Card className="mb-3" bg={item.isComplete ? 'success' : ''}>
         <Card.Body>
-          <Card.Title>{item.task}</Card.Title>
+          <Card.Title className="text-truncate-title-multiline">
+            {item.task}
+          </Card.Title>
           <Card.Subtitle className="mt-3 mb-3">
             {formatDate(item.dueStartDate)} ~ {formatDate(item.dueEndDate)}
           </Card.Subtitle>
