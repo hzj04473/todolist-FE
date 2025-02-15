ea665c57c97f6ffe6b62022758adf31f61241ef1
hzj04473 <hzj04473@naver.com>
2025-02-16 08:10:38 +0900

    0216 App.js HelmetProvider 작업

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useCallback, useEffect, useState } from 'react';
 import PrivateRoute from './route/PrivateRoute';
 import api from './utils/api';
 import { Container } from 'react-bootstrap'; // 추가
+import { HelmetProvider } from 'react-helmet-async'; // ✅ HelmetProvider 추가
 
 function App() {
   const [user, setUser] = useState(null);
@@ -28,56 +29,58 @@ function App() {
   }, [getUser]);
 
   return (
-    <Container fluid className="p-0">
-      {/* 추가: 전체 레이아웃을 Container로 감싸기 */}
-      <Routes>
-        {/* Private Router */}
-        {/* 메인페이지 */}
-        <Route
-          path="/"
-          element={
-            <PrivateRoute user={user}>
-              <TodoPage user={user} setUser={setUser} />
-            </PrivateRoute>
-          }
-        />
-        {/* 검색할떄 */}
-        <Route
-          path="/search/:keyword"
-          element={
-            <PrivateRoute user={user}>
-              <TodoPage user={user} setUser={setUser} />
-            </PrivateRoute>
-          }
-        />
-        {/* 회원가입 / 수정 */}
-        <Route
-          path="/register"
-          element={<RegisterPage user={user} setUser={setUser} />}
-        />
+    <HelmetProvider>
+      <Container fluid className="p-0">
+        {/* 추가: 전체 레이아웃을 Container로 감싸기 */}
+        <Routes>
+          {/* Private Router */}
+          {/* 메인페이지 */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute user={user}>
+                <TodoPage user={user} setUser={setUser} />
+              </PrivateRoute>
+            }
+          />
+          {/* 검색할떄 */}
+          <Route
+            path="/search/:keyword"
+            element={
+              <PrivateRoute user={user}>
+                <TodoPage user={user} setUser={setUser} />
+              </PrivateRoute>
+            }
+          />
+          {/* 회원가입 / 수정 */}
+          <Route
+            path="/register"
+            element={<RegisterPage user={user} setUser={setUser} />}
+          />
 
-        {/* 로그인페이지 */}
-        <Route
-          path="/login"
-          element={<LoginPage user={user} setUser={setUser} />}
-        />
+          {/* 로그인페이지 */}
+          <Route
+            path="/login"
+            element={<LoginPage user={user} setUser={setUser} />}
+          />
 
-        {/* 카카오톡 Redirect_url */}
-        <Route
-          path="/auth/kakao/callback"
-          element={<RegisterPage snsLoginType={`kakao`} />}
-        />
+          {/* 카카오톡 Redirect_url */}
+          <Route
+            path="/auth/kakao/callback"
+            element={<RegisterPage snsLoginType={`kakao`} />}
+          />
 
-        <Route
-          path="*"
-          element={
-            <PrivateRoute user={user}>
-              <TodoPage user={user} setUser={setUser} />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Container>
+          <Route
+            path="*"
+            element={
+              <PrivateRoute user={user}>
+                <TodoPage user={user} setUser={setUser} />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Container>
+    </HelmetProvider>
   );
 }
 
